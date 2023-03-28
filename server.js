d9a2f835909db26f82f46ad75c46f3b18eb04fb4
郵差 <twHenryYang@gmail.com>
2023-03-28 22:05:34 +0800

    feat: Real HL7 message example for testing

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ server.on('hl7', function (data) {
 
 setTimeout(() => {
   // Send outbound messages
-  server.send('127.0.0.1', 1234, 'outbound-hl7-message', function (err, ackData) {
-    // async callback code here
-  });
+  server.send('127.0.0.1', 1234,
+    `MSH|^~\&|HMIS|KIWI_TEAM|PACS|DCM4CHEE|20230307171125||OMI^O23^OMI_O23|20230307171125|P|2.5.1||||||UNICODE UTF-8
+    PID|||A223456789^^^Issuer^NH~DDS-30939^^^DDS&1.3.2^PI||PatientName||19760612201400|M
+    ORC|NW|12028^IHE_OM_OP^1.2.20230307.17.10.1^ISO|12850^IHE_OM_OF^1.2.20230307.17.10.2^ISO||SC
+    OBR|1|12028^IHE_OM_OP^1.2.20230307.17.10.1^ISO|||||||||||||||||OMRP202303071710|||||CT`
+    , function (err, ackData) {
+      // async callback code here
+    });
 }, 3000);
