67b2c1a481a20d07fed031c5908cca87edde5359
郵差 <twHenryYang@gmail.com>
2023-03-28 21:55:07 +0800

    update: Send test data after starting server for 3s

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const mllp = require('mllp-node');
- 
+
 var server = new mllp.MLLPServer('127.0.0.1', 1234);
- 
+
+console.log("HL7 MLLP server is listening on 127.0.0.1:1234");
 // Subscribe to inbound messages
-server.on('hl7', function (data){
-    console.log('received payload:', data);
+server.on('hl7', function (data) {
+  console.log('received payload:', data);
 });
- 
-// Send outbound messages
-server.send('127.0.0.1', 4321, 'outbound-hl7-message', function (err, ackData) {
+
+
+setTimeout(() => {
+  // Send outbound messages
+  server.send('127.0.0.1', 1234, 'outbound-hl7-message', function (err, ackData) {
     // async callback code here
-});
+  });
+}, 3000);
